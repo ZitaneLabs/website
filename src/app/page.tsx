6ff2bf691afdfb4062a27682b699cca4e0e45ed6
SplittyDev <splittydev@protonmail.com>
2023-08-30 14:36:30 +0200

    Improve mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,18 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-8 p-8 md:p-24 text-white">
       <div className="flex flex-col items-center">
-        <h1 className="text-4xl sm:text-6xl md:text-8xl font-bold text-center flex gap-2">
+        <h1 className="text-5xl sm:text-6xl md:text-8xl font-bold text-center flex gap-2">
           <span className="text-gray-200">Zitane</span>
           <span className="text-white font-bold">Labs</span>
         </h1>
-        <h2 className="uppercase text-lg">
+        <h2 className="uppercase text-xs sm:text-sm md:text-lg text-center">
           Eestis valmistatud Saarimaa tarkvara
         </h2>
       </div>
       <div className="flex justify-center gap-4">
-        <img className="object-contain h-24 justify-center drop-shadow-md" src="/assets/estonia.svg" />
-        <img className="object-contain h-24 drop-shadow-md" src="/assets/saarland.svg" />
+        <img className="object-contain h-16 md:h-24 justify-center drop-shadow-md" src="/assets/estonia.svg" />
+        <img className="object-contain h-16 md:h-24 drop-shadow-md" src="/assets/saarland.svg" />
       </div>
     </main>
   )
